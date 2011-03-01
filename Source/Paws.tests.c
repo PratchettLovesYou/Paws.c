c4be8dec76b44773d488af5a155337171570342b
elliottcable <me@ell.io>
2011-02-28 23:50:58 -0500

    (new tests) Adding a `main()` in `Paws.tests.c` that runs the test suite.

---
+++ Source/Paws.tests.c
@@ -0,0 +1,18 @@
+# include "Paws.c"
+/*  This is the root “tests file.” You can use this to run all of the Paws.c tests, by compiling against `Cest.c`
+ *  with `CEST__NO_AUTO` defined:
+ *      
+ *      C99 -DCEST__NO_AUTO ../Cest.c/Source/Cest.c -ISource \
+ *        Source/Types/fork/fork.tests.c \
+ *        Source/Paws.tests.c \
+ *      && ./Paws.tests.o
+ *      
+ */
+
+# define DECLARATIONS
+#   include "../../Cest.c/Source/Cest.c"
+# undef  DECLARATIONS
+
+// This simply constructs the Paws environment (incase you’re not building on `clang` and
+// `__attribute__((constructor))` isn’t available... see the discussion in `Paws.c` at `construct()`)
+int main() { construct(); return Cest.run_all(); }
